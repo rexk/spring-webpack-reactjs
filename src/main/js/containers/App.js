b598d84247a92211af2c38b96eae69eea5810664
rexk <myungsunrex@gmail.com>
2015-09-21 22:37:56 -0700

    Add Top-Level React App

---
+++ src/main/js/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  static displayName = 'App';
+
+  render() {
+    return (
+      <div>Hello App</div>
+    );
+  }
+}
+
+export default App;
--- src/main/js/mountApp.js
+++ src/main/js/mountApp.js
@@ -1,3 +1,19 @@
-console.log('mount me');
-console.log('changed man');
-document.getElementById('content').innerHTML = 'hello';
+import React from 'react';
+import App from './containers/App';
+
+export default function mountApp() {
+  const mountNode = document.getElementById('root');
+  React.render(
+    <App />,
+    mountNode,
+    function renderCallback(err) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('mounted');
+    }
+  );
+}
+
+mountApp();
